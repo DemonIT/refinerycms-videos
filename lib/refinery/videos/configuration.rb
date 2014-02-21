20d58716308796dd106e1853b791a683eea015bf
demonIT <crazy635@gmail.com>
2014-02-21 16:55:08 +0400

    Version '1.0.5' Add min_view_size

--- lib/refinery/videos/configuration.rb
+++ lib/refinery/videos/configuration.rb
@@ -4,7 +4,8 @@ module Refinery
 
     config_accessor :datastore_root_path, :whitelisted_mime_types, :max_file_size, :file_prefix, :pages_per_dialog,
                     :pages_per_admin_index, :datastore_path, :skin_css_class,:video_default_width, :video_default_height,
-                    :poster_mime_types, :poster_max_file_limit, :poster_datastore_path, :poster_datastore_root_path, :poster_file_prefix
+                    :poster_mime_types, :poster_max_file_limit, :poster_datastore_path, :poster_datastore_root_path, :poster_file_prefix,
+                    :view_min_width, :view_min_height
 
     # videos
     self.max_file_size = 104857600
@@ -15,6 +16,8 @@ module Refinery
     self.video_default_width = 375
     self.video_default_height = 255
     self.datastore_path = 'system/refinery/videos'
+    self.view_min_width = 300
+    self.view_min_height = 200
 
     #poster
     self.poster_file_prefix = 'vPoster'
